67e4cfcd56647c91c5b6f8198a2aa6703727f90d
Pratik4230 <pratikjadhav1438@gmail.com>
2024-01-10 16:34:14 +0530

    Foundation

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement("h1",{ id : "heading"} , "added a header" );
- const root =  ReactDOM.createRoot(document.getElementById('root'));
- root.render(heading);
+
+ const JsxHeading = () =>  (
+ <h1 className='heading'>
+ this is jsx
+  in  
+  multuple line</h1> );
+   
+
+   //functional component
+   const FunctCompo = () => (
+    <div> 
+        {/* component composition = compo in compo */}
+        {JsxHeading()}
+         <JsxHeading/>
+         <JsxHeading></JsxHeading>
+         <h2>{ 4 + 3 }</h2>
+        <h1> function that return Jsx  Element </h1>
+    </div> );
+
+
+ const root = ReactDOM.createRoot(document.getElementById('root'))
+
+ root.render(<FunctCompo/>);
