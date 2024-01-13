49dcc13c827587f6a71b7ae7cfd5e0d09a85e1dc
Pratik4230 <pratikjadhav1438@gmail.com>
2024-01-13 19:53:52 +0530

    Problem solved

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,31 +5,26 @@ import {resList}  from "../utils/mockData";
 
 const Body = () => {
 
-
-// const [resList, setResList] = useState(resList);
-
+    // const [resList, setresList] = useState(resList);  X
+const [ResList, setResList] = useState(resList);
 
     return (
         <main className='body'>
             <div> 
                 <button className="btn" onClick={ () => {
-                    let updated = resList.filter( (res) => res.info.sla.deliveryTime < 25 );
-                //  setResData(updated);
+                    let updated = ResList.filter( (res) => res.info.sla.deliveryTime < 25 );
+                 setResList(updated);
                  console.log(updated);
                 } 
-                
                 } >This is Button</button>
-
-                
             </div>
 
       <section className='RestaurantContainer'>
       
-     {resList.map( (re) => (
+     {ResList.map( (re) => (
       <RestaurantCard key ={re.info.id} resData={re} />
      ) )} 
       </section>
-
         </main>
     )
 }
