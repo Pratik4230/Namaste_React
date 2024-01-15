267ee8b07aaa901dc28157732074dc51d314238d
Pratik4230 <pratikjadhav1438@gmail.com>
2024-01-15 09:47:40 +0530

    used swiggy api

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,27 +1,41 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {resList}  from "../utils/mockData";
 
 
 const Body = () => {
 
     // const [resList, setresList] = useState(resList);  X
-const [ResList, setResList] = useState(resList);
+const [gotList, setGotList] = useState(resList);
+
+useEffect( () => {
+    console.log("got it")
+    fetchData();
+} , [] );
+
+ const fetchData = async () => {
+  const data = await fetch("https://www.swiggy.com/mapi/homepage/getCards?lat=18.5204303&lng=73.8567437");
+  const gotData = await data.json();
+  console.log(gotData);
+  setGotList(gotData.data.success.cards[4].gridWidget.gridElements.infoWithStyle.restaurants);
+  }
+//   const data = fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING");
+
+
 
     return (
         <main className='body'>
             <div> 
                 <button className="btn" onClick={ () => {
-                    let updated = ResList.filter( (res) => res.info.sla.deliveryTime < 25 );
-                 setResList(updated);
+                    let updated = gotList.filter( (res) => res.info.sla.deliveryTime < 25 );
+                 setGotList(updated);
                  console.log(updated);
                 } 
                 } >This is Button</button>
             </div>
 
       <section className='RestaurantContainer'>
-      
-     {ResList.map( (re) => (
+{gotList.map( (re) => (
       <RestaurantCard key ={re.info.id} resData={re} />
      ) )} 
       </section>
--- src/utils/mockData.js
+++ src/utils/mockData.js
@@ -1831,3 +1831,4 @@ export const resList =   [
   }
 ];
 
+// https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING
