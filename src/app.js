92dad50f49727878d7c0c5473b79d9b031b79f42
Pratik4230 <pratikjadhav1438@gmail.com>
2024-01-28 10:16:34 +0530

    Lazy loading

--- src/app.js
+++ src/app.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { lazy , Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './components/Header';
 import Body from  './components/Body';
-import About from './components/About';
+// import About from './components/About';
 import Contact from './components/Contact';
 import Error from './components/Error';
 import { createBrowserRouter , RouterProvider , Outlet} from 'react-router-dom';
 import ResMenu from './components/ResMenu';
 
+//chunking //Code Splitting //Dynamic Bundling  //Lazy Loading  //on demand Loading // dynamix import
 
-
+const About = lazy( () =>  import('./components/About')  );
 
 const AppLayout = () => {
     return (
@@ -28,12 +29,12 @@ const appRouter = createBrowserRouter([
     children : [
       {
         path : '/',
-        element : <Body/>
+        element : <Suspense fallback={ <h1>this is Suspence</h1> } >  <Body/>  </Suspense>
     },
 
         {
           path : '/about',
-          element : <About/>
+          element :  <Suspense fallback={ <h1>this is Suspence</h1> } >  <About/>  </Suspense>
       },
         {
         path : '/contact',
